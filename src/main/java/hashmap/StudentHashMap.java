52246edd186ae0f7bc195ee08c76d85710494b22
shireeshakavali80 <shireeshakavali80@gmail.com>
2024-01-29 09:42:37 -0600

    Maven Project

--- src/main/java/hashmap/StudentHashMap.java
+++ src/main/java/hashmap/StudentHashMap.java
@@ -1,5 +1,7 @@
 package hashmap;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 
 public class StudentHashMap {
@@ -61,6 +63,46 @@ public class StudentHashMap {
                     ", lastName='" + lastName + '\'' +
                     ", GPA=" + GPA +
                     '}';
+        }
+    }
+
+    public static class HashSetIteration {
+
+        public static void main(String[] args) {
+            // Create a HashSet
+            HashSet<String> stringSet = new HashSet<>();
+
+            // Add 10 strings to the HashSet
+            stringSet.add("Apple");
+            stringSet.add("Banana");
+            stringSet.add("Orange");
+            stringSet.add("Grapes");
+            stringSet.add("Pineapple");
+            stringSet.add("Strawberry");
+            stringSet.add("Mango");
+            stringSet.add("Peach");
+            stringSet.add("Watermelon");
+            stringSet.add("Kiwi");
+
+            // Iterate over the HashSet using Iterator
+            System.out.println("Iterating over HashSet using Iterator:");
+            Iterator<String> iterator = stringSet.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+            // Iterate over the HashSet using for loop
+            System.out.println("\nIterating over HashSet using for loop:");
+            Object[] array = stringSet.toArray();
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+            }
+
+            // Iterate over the HashSet using enhanced for loop
+            System.out.println("\nIterating over HashSet using enhanced for loop:");
+            for (String str : stringSet) {
+                System.out.println(str);
+            }
         }
     }
 }
---
+++ src/test/java/hashmapTest/RemoveDuplicatesFromStringTest.java
@@ -0,0 +1,32 @@
+package hashmapTest;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import hashmap.RemoveDuplicatesFromString;
+
+public class RemoveDuplicatesFromStringTest {
+
+    @Test
+    public void testRemoveDuplicates() {
+        RemoveDuplicatesFromString remover = new RemoveDuplicatesFromString();
+
+        // Test with string containing duplicates
+        String stringWithDuplicates = "hello";
+        assertEquals("helo", remover.removeDuplicates(stringWithDuplicates));
+
+        // Test with string containing no duplicates
+        String stringWithoutDuplicates = "world";
+        assertEquals("world", remover.removeDuplicates(stringWithoutDuplicates));
+
+        // Test with empty string
+        String emptyString = "";
+        assertEquals("", remover.removeDuplicates(emptyString));
+
+        // Test with string containing all duplicate characters
+        String allDuplicates = "aaaa";
+        assertEquals("a", remover.removeDuplicates(allDuplicates));
+
+        // Test with string containing mixed characters
+        String mixedChars = "abracadabra";
+        assertEquals("abrcd", remover.removeDuplicates(mixedChars));
+    }
+}
