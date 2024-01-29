166b6784e9b172aad6f4552311986f4e71013fe3
shireeshakavali80 <shireeshakavali80@gmail.com>
2024-01-28 22:19:11 -0600

    Maven Project

---
+++ src/main/java/finalkeyword/Book.java
@@ -0,0 +1,17 @@
+package finalkeyword;
+
+public class Book {
+    // Final instance variable in the final class
+    final String bookTitle;
+
+    // Constructor to initialize the final variable
+    public Book(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    // Final method to display book details
+    final void displayBookDetails() {
+        System.out.println("Book Title: " + bookTitle);
+        // Additional book details can be displayed here
+    }
+}
